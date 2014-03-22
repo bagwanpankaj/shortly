4fd068d8839098ab644387f90d1c782dc972a25e
bagwanpankaj <bagwanpankaj@gmail.com>
2014-03-22 19:43:46 +0530

    [ADD] updated

--- Gemfile
+++ Gemfile
@@ -1,14 +1,11 @@
 source "http://rubygems.org"
 
 gem "httparty"
-gem 'json'
+gem "json"
 
-# Add dependencies to develop your gem here.
-# Include everything needed to run rake, tests, features, etc.
 group :development do
   gem "rake"
   gem "rspec"
   gem "bundler"
   gem 'fakeweb'
-  gem "debugger"
 end
--- spec/shortly_spec.rb
+++ spec/shortly_spec.rb
@@ -12,31 +12,31 @@ describe "Shortly" do
       @uri = @googl.base_uri + @googl.send(:relative_path)
     end
     
-    it "should get a short url from googl(provided valid url)" do
+    it ".shorten" do
       mock_request_for(@uri, :method => :post, :fixture => "googl_shorten")
       res = @googl.shorten(@long_url)
       res.shortUrl.should_not be_empty
       res.shortUrl.should == @short_url
     end
     
-    it "result of shorten should be an instance of OpenStruct" do
+    it ".shorten should respond_with OpenStruct" do
       res = @googl.shorten(@long_url)
       res.should be_an_instance_of(OpenStruct)
     end
     
-    it "should get a long url back for given valid short url" do
+    it ".expand" do
       mock_request_for(@uri + "?shortUrl=#{CGI.escape(@short_url)}", :method => :get, :fixture => "googl_expand")
       res = @googl.expand(@short_url)
       res.longUrl.should_not be_empty
       res.longUrl.should == @long_url
     end
     
-    it "result of expand should be an instance of OpenStruct" do
+    it ".expand should respond_with OpenStruct" do
       res = @googl.expand(@short_url)
       res.should be_an_instance_of(OpenStruct)
     end
     
-    it "should give analytics for given short url" do
+    it ".analytics" do
       mock_request_for(@uri + "?shortUrl=#{CGI.escape(@short_url)}&projection=FULL",
         :fixture => "googl_analytics")
       
@@ -47,7 +47,7 @@ describe "Shortly" do
       res.analytics["day"].should be_an_instance_of(Hash)
     end
     
-    it "should throw an error on wrong uri format" do
+    it "InvalidURIError" do
       lambda do
         @googl.shorten(@invalid_url)
       end.should raise_error(Shortly::Errors::InvalidURIError)
@@ -65,7 +65,7 @@ describe "Shortly" do
       @uri = @isgd.base_uri
     end
     
-    it "should get a short url from Is.gd(provided valid url)" do
+    it ".shorten" do
       mock_request_for(@uri + "/create.php?url=#{CGI.escape(@long_url)}&format=json",
         :fixture => "isgd_shorten")
       
@@ -74,18 +74,18 @@ describe "Shortly" do
       res.shorturl.should == @shorturl
     end
     
-    it "result should be an instance of OpenStruct" do
+    it ".shorten should respond_with OpenStruct" do
       res = @isgd.shorten(@long_url)
       res.should be_an_instance_of(OpenStruct)
     end
     
-    it "should throw an error on wrong uri format" do
+    it "InvalidURIError" do
       lambda do
         @isgd.shorten(@invalid_url)
       end.should raise_error(Shortly::Errors::InvalidURIError)
     end
     
-    it "should raise MethodNotAvailableError if method is not implemented for" do
+    it "MethodNotAvailableError" do
       lambda do
         @isgd.expand(@long_url)
       end.should raise_error(Shortly::Errors::MethodNotAvailableError)
@@ -93,40 +93,40 @@ describe "Shortly" do
   end
   
   #tests for client vgd
-   describe "Vgd" do
-     before(:all) do
-       @vgd = Shortly::Clients::Vgd
-       @long_url = "http://bagwanpankaj.com"
-       @invalid_url = "bagwanpankaj.com"
-       @uri = @vgd.base_uri
-     end
+  describe "Vgd" do
+    before(:all) do
+      @vgd = Shortly::Clients::Vgd
+      @long_url = "http://bagwanpankaj.com"
+      @invalid_url = "bagwanpankaj.com"
+      @uri = @vgd.base_uri
+    end
   
-     it "should get a short url from v.gd(provided valid url)" do
-       mock_request_for(@uri + "/create.php?url=#{CGI.escape(@long_url)}&format=json", 
-        :fixture => "vgd_shorten")
-       
-       res = @vgd.shorten(@long_url)
-       res.shorturl.should_not be_empty
-       res.shorturl.should == @vgd.shorten(@long_url).shorturl
-     end
+    it ".shorten" do
+      mock_request_for(@uri + "/create.php?url=#{CGI.escape(@long_url)}&format=json", 
+      :fixture => "vgd_shorten")
+     
+      res = @vgd.shorten(@long_url)
+      res.shorturl.should_not be_empty
+      res.shorturl.should == @vgd.shorten(@long_url).shorturl
+    end
   
-     it "result should be an instance of OpenStruct" do
-       res = @vgd.shorten(@long_url)
-       res.should be_an_instance_of(OpenStruct)
-     end
+    it ".shorten should respond_with OpenStruct" do
+      res = @vgd.shorten(@long_url)
+      res.should be_an_instance_of(OpenStruct)
+    end
   
-     it "should throw an error on wrong uri format" do
-       lambda do
-         @vgd.shorten(@invalid_url)
-       end.should raise_error(Shortly::Errors::InvalidURIError)
-     end
+    it "InvalidURIError" do
+      lambda do
+        @vgd.shorten(@invalid_url)
+      end.should raise_error(Shortly::Errors::InvalidURIError)
+    end
   
-     it "should raise MethodNotAvailableError if method is not implemented for" do
-       lambda do
-         @vgd.expand(@long_url)
-       end.should raise_error(Shortly::Errors::MethodNotAvailableError)
-     end
-   end
+    it "MethodNotAvailableError" do
+      lambda do
+        @vgd.expand(@long_url)
+      end.should raise_error(Shortly::Errors::MethodNotAvailableError)
+    end
+  end
   
   #tests for client bitly
   describe "Bitly" do
@@ -141,7 +141,7 @@ describe "Shortly" do
       @uri = @bitly.base_uri + "/v3"
     end
     
-    it "should get a short url from bitly(provided valid url)" do
+    it ".shorten" do
       mock_request_for(@uri + "/shorten?longUrl=#{CGI.escape(@long_url)}&format=json&login=#{@bitly.login}&apiKey=#{@bitly.apiKey}", 
        :fixture => "bitly_shorten")
        
@@ -150,18 +150,18 @@ describe "Shortly" do
       res.url.should == @short_url
     end
     
-    it "result should be an instance of OpenStruct" do
+    it ".shorten should respond_with OpenStruct" do
       res = @bitly.shorten(@long_url)
       res.should be_an_instance_of(OpenStruct)
     end
     
-    it "should throw an error on wrong uri format" do
+    it "InvalidURIError" do
       lambda do
         @bitly.shorten(@invalid_url)
       end.should raise_error(Shortly::Errors::InvalidURIError)
     end
     
-    it "should expand a given short url" do
+    it ".expand" do
       mock_request_for(@uri + "/expand?shortUrl=#{CGI.escape(@short_url)}&format=json&login=#{@bitly.login}&apiKey=#{@bitly.apiKey}", 
        :fixture => "bitly_expand")
        
@@ -184,7 +184,7 @@ describe "Shortly" do
       @uri = @jmp.base_uri + '/v3'
     end
     
-    it "should get a short url from jmp(provided valid url)" do
+    it ".shorten" do
       mock_request_for(@uri + "/shorten?longUrl=#{CGI.escape(@long_url)}&format=json&login=#{@jmp.login}&apiKey=#{@jmp.apiKey}", 
        :fixture => "jmp_shorten")
        
@@ -193,18 +193,18 @@ describe "Shortly" do
       res.url.should == @short_url
     end
     
-    it "result should be an instance of OpenStruct" do
+    it ".shorten should respond_with OpenStruct" do
       res = @jmp.shorten(@long_url)
       res.should be_an_instance_of(OpenStruct)
     end
     
-    it "should throw an error on wrong uri format" do
+    it "InvalidURIError" do
       lambda do
         @jmp.shorten(@invalid_url)
       end.should raise_error(Shortly::Errors::InvalidURIError)
     end
     
-    it "should expand a given short url" do
+    it ".expand" do
       mock_request_for(@uri + "/expand?shortUrl=#{CGI.escape(@short_url)}&format=json&login=#{@jmp.login}&apiKey=#{@jmp.apiKey}", 
        :fixture => "jmp_expand")
        
@@ -222,7 +222,7 @@ describe "Shortly" do
       @uri = Shortly::Clients::Tinyurl.base_uri
     end
     
-    it "should get a short url from TinyUrl(provided valid url)" do
+    it ".shorten" do
       mock_request_for(@uri + "/api-create.php", :method => :post,
        :fixture => "tinyurl_shorten", :content_type => "text/plain")
        
@@ -231,18 +231,18 @@ describe "Shortly" do
       res.shorturl.should == "http://tinyurl.com/6jt3pjr"
     end
     
-    it "result should be an instance of OpenStruct" do
+    it ".shorten should respond_with OpenStruct" do
       res = Shortly::Clients::Tinyurl.shorten(@long_url)
       res.should be_an_instance_of(OpenStruct)
     end
     
-    it "should throw an error on wrong uri format" do
+    it "InvalidURIError" do
       lambda do
         Shortly::Clients::Tinyurl.shorten(@invalid_url)
       end.should raise_error(Shortly::Errors::InvalidURIError)
     end
     
-    it "should raise MethodNotAvailableError if method is not implemented for" do
+    it "should raise MethodNotAvailableError" do
       lambda do
         Shortly::Clients::Tinyurl.expand(@long_url)
       end.should raise_error(Shortly::Errors::MethodNotAvailableError)
@@ -261,7 +261,7 @@ describe "Shortly" do
       @uri = @ss.base_uri
     end
 
-    it "should get a short url from ShortSwitch(provided valid url)" do
+    it ".shorten" do
       mock_request_for(@uri + "/shorten?longUrl=#{CGI.escape(@long_url)}&format=json&apiKey=TESTKEY",
        :fixture => "shortswitch_shorten")
     
@@ -270,12 +270,12 @@ describe "Shortly" do
       res.shortUrl.should == @short_url
     end
     
-    it "result should be an instance of OpenStruct" do
+    it ".shorten should respond_with OpenStruct" do
       res = Shortly::Clients::ShortSwitch.shorten(@long_url)
       res.should be_an_instance_of(OpenStruct)
     end
     
-    it "should throw an error on wrong uri format" do
+    it "InvalidURIError" do
       lambda do
         Shortly::Clients::ShortSwitch.shorten(@invalid_url)
       end.should raise_error(Shortly::Errors::InvalidURIError)
@@ -295,7 +295,7 @@ describe "Shortly" do
       @uri = @snim.base_uri
     end
     
-    it "should get a short url from Snim(provided valid url)" do
+    it ".shorten" do
       mock_request_for(@uri + "/getsnip", :method => :post,
        :fixture => "snim_shorten")
     
@@ -304,7 +304,7 @@ describe "Shortly" do
       res.shortUrl.should == @short_url
     end
     
-    it "should get analytics from Snim(provided valid id)" do
+    it ".analytics" do
       mock_request_for(@uri + "/getsnipdetails", :method => :post,
        :fixture => "snim_analytics")
     
@@ -314,7 +314,7 @@ describe "Shortly" do
       res.clicks.should == 10000
     end
     
-    it "should get long url from Snim(provided valid shorturl)" do
+    it ".expand" do
       mock_request_for(@uri + "/getsnipdetails", :method => :post,
        :fixture => "snim_analytics")
     
@@ -331,29 +331,29 @@ describe "Shortly" do
       @invalid_uri = "someinveliduri" 
     end
     
-    it "should include HTTParty" do
+    it "includes HTTParty" do
       Shortly::Client.included_modules.should include HTTParty
     end
     
-    it "should include Shortly::Errors" do
+    it "includes Shortly::Errors" do
       Shortly::Client.included_modules.should include Shortly::Errors
     end
     
-    it "should include Shortly::Helper" do
+    it "includes Shortly::Helper" do
       Shortly::Client.included_modules.should include Shortly::Helper
     end
     
-    it "should respond to resitered class variable" do
+    it ":@@registered" do
       Shortly::Client.class_variables.should include :"@@registered"
     end
     
-    it "should respond to register/valid_uri? and registered methods" do
+    it ".register/.valid_uri?/.registered" do
       Shortly::Client.should respond_to :register!
       Shortly::Client.should respond_to :registered
       Shortly::Client.should respond_to :valid_uri?
     end
     
-    it "should test a uri is valid or not" do
+    it ".valid_uri" do
       Shortly::Client.valid_uri?(@valid_uri).should be true
       Shortly::Client.valid_uri?(@invalid_uri).should_not be true
     end
@@ -364,7 +364,7 @@ describe "Shortly" do
     
     describe "Errors" do
       
-      it "should have various error classes implemented" do
+      it "CustomErrors" do
         Shortly::Errors::ShortlyError.superclass.name.should == "StandardError"
         Shortly::Errors::ShortlyArgumentError.superclass.name.should == "ArgumentError"
         Shortly::Errors::InvalidURIError.superclass.name.should == "Shortly::Errors::ShortlyError"
@@ -375,11 +375,11 @@ describe "Shortly" do
     end
     
     describe "Shortly" do
-      it "should return current version of gem" do
+      it ".version" do
         Shortly.version.should == File.read(File.join(File.dirname(__FILE__), '..', 'VERSION'))
       end
       
-      it "should return all registered services" do
+      it ".active_services" do
         Shortly.active_services.should == Shortly::Client.registered
       end
       
